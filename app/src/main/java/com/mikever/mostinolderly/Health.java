9ad920815a1824668259de0308749467c8c9c90a
Benny <circlebenny@gmail.com>
2017-02-05 17:17:28 +0800

    5th Feb at Benny Home test recovered

--- app/src/main/java/com/mikever/mostinolderly/Health.java
+++ app/src/main/java/com/mikever/mostinolderly/Health.java
@@ -13,7 +13,7 @@ public class Health extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health);
 
-
+//        666
 
 //        555
 
